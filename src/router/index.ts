19010b097ebf1a1d2ed228d9d97676f244b3f474
feige996 <1020102647@qq.com>
2025-09-02 20:00:31 +0800

    fix(router): 修复路由基础路径配置问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '@/views/Dashboard.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/beacon/'),
   routes: [
     {
       path: '/',
