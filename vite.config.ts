ccd7fa58a33d4d45eab41e63b4cb188bb832e5dd
feige996 <1020102647@qq.com>
2025-09-02 18:17:38 +0800

    build: 添加基础路径配置为'/beacon/'

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), UnoCSS()],
+  base: '/beacon/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
